c5a1793ef95736e622c7541d74579dc1a0ae6906
Mohammad Sadjad Fallah <m.sadjad.fallah@gmail.com>
2014-04-11 09:09:06 +0430

    the login page view is ready

--- application/views/login.php
+++ application/views/login.php
@@ -22,7 +22,7 @@
                 <label for="exampleInputPassword1">Password</label>
                 <input type="password" class="form-control" id="password" placeholder="Password goes here">
             </div>
-            <button type="submit" class="btn btn-default">Log me in please</button>
+            <button type="submit" class="btn btn-warning btn-lg btn-border">Log me in please</button>
         </form>
     </div>
 </div>
