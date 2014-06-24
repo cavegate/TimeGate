873b5c37250aa2567d5089026c03b3044204d059
Mohammad Sadjad Fallah <m.sadjad.fallah@gmail.com>
2014-06-24 11:56:51 +0430

    template for dashboard

--- application/controllers/dashboard.php
+++ application/controllers/dashboard.php
@@ -15,9 +15,18 @@ class Dashboard extends CI_Controller {
 
     public function index()
     {
-        echo "<pre>";
-        echo $this->input->post();
-        echo "</pre>";
+        //Todo: create a dashboard controller and show it here
+        /*
+         * Description:
+         * -This controller must be restricted by not logged in users
+         * -Show the number of hours that the user is working
+         * -If the user is not working allow them to work
+         * -Allow manual log outs
+         * -Allow manual log ins
+         */
     }
+
+    public function calculateLoginStatus()
+    {}
 }
 
--- application/models/login_model.php
+++ application/models/login_model.php
@@ -8,13 +8,21 @@
 
 class Login_model extends CI_Model
 {
+    /**
+     * Checks for the login status whether the username password are valid or not
+     * @param $username
+     * @param $password
+     * @return bool|resource
+     */
     public static function checkLogin($username, $password)
     {
         $queryString = "select * from users where username = '$username' and password = '$password'";
-        echo $queryString."<br/>";
+        //echo $queryString."<br/>";
         $result = mysql_query($queryString);
-        print_r($result);
+        //print_r($result);
         if(count($result)>0)
             return $result;
+        else
+            return false;
     }
 } 
