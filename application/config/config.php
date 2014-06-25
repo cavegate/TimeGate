f98b0a3006e681cc5a82c9b64a8292dc4a9c589e
ehsanh <houshmand2007@gmail.com>
2014-06-25 16:44:32 +0330

    register
    Save register form's data to database

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://localhost:1443/timegate/';
+$config['base_url']	= 'http://localhost/timegate/';
 
 /*
 |--------------------------------------------------------------------------
